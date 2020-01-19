836b97313261981de3264f2f24ca6e65d6610725
Bjverde <bjverde@yahoo.com.br>
2020-01-19 17:37:47 -0300

    :hammer: tela do gerador de grafo

---
+++ www/app/control/telas/GeraGrafoForm.php
@@ -0,0 +1,26 @@
+<?php
+
+class GeraGrafoForm extends TPage
+{
+    private $form;
+    
+    /**
+     * Class constructor
+     * Creates the page
+     */
+    function __construct()
+    {
+        parent::__construct();
+
+        $this->form = new BootstrapFormBuilder;
+        $this->form->setFormTitle('Gerador de Grafó');
+        $this->form->generateAria(); // automatic aria-label
+
+        // wrap the page content using vertical box
+        $vbox = new TVBox;
+        $vbox->style = 'width: 100%';
+        $vbox->add(new TXMLBreadCrumb('menu.xml', __CLASS__));
+        $vbox->add($this->form);
+        parent::add($vbox);
+    }
+}
