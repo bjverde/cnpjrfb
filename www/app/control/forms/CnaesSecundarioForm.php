c692bfdbbb68d3ea6fef940ab6e737fc7970b07b
Bjverde <bjverde@yahoo.com.br>
2020-01-27 23:45:56 -0300

    :hammer: tela do CNAE

--- www/app/control/forms/CnaesSecundarioForm.php
+++ www/app/control/forms/CnaesSecundarioForm.php
@@ -2,36 +2,79 @@
 
 class CnaesSecundarioForm extends TPage
 {
-    private $form;     // registration form
-    private $datagrid; // listing
+    protected $form; // registration form
+    protected $datagrid; // listing
+    protected $pageNavigation;
 
-    // trait with onSave, onClear, onEdit
-    use Adianti\Base\AdiantiStandardFormTrait;
+    // trait com onReload, onSearch, onDelete...
+    use Adianti\Base\AdiantiStandardListTrait;
     
     public function __construct()
     {
         parent::__construct();
 
-        $this->form = new BootstrapFormBuilder;
+        $this->setDatabase('cnpj_full'); // define the database
+        $this->setActiveRecord('CnaesSecundario'); // define the Active Record
+        $this->addFilterField('cnpj', 'cnae_ordem', 'cnae'); //campo, operador, campo do form
+        $this->setDefaultOrder('cnpj', 'asc'); // define the default order
+
+        $this->form = new BootstrapFormBuilder(__CLASS__);
         $this->form->setFormTitle('CNAE');
         $this->form->generateAria(); // automatic aria-label
 
 
         $cnpj = new TEntry('cnpj');
-        $cnae = new TEntry('CNAE');
+        $cnae = new TEntry('cnae');
         $cnae_ordem = new TEntry('cnae_ordem');
 
         $this->form->addFields( [new TLabel('CNPJ')],[$cnpj]);
         $this->form->addFields( [new TLabel('cnae')],[$cnae]);
         $this->form->addFields( [new TLabel('Ordem')],[$cnae_ordem]);
 
-        $this->form->addActionLink('Limpar',  new TAction(array($this, 'onClear')), 'fa:eraser red');
+        // add form actions
+        $this->form->addAction('Find', new TAction([$this, 'onSearch']), 'fa:search blue');        
+        $this->form->addActionLink('Clear',  new TAction([$this, 'clear']), 'fa:eraser red');
 
-        // wrap the page content using vertical box
+        // keep the form filled with the search data
+        $this->form->setData( TSession::getValue('StandardDataGridView_filter_data') );
+
+        // create the datagrid
+        $this->datagrid = new BootstrapDatagridWrapper(new TDataGrid);
+        $this->datagrid->width = '100%';
+        
+        // add the columns
+        $col_cnpj        = new TDataGridColumn('cnpj', 'CNPJ', 'right','10%');
+        $col_cnae        = new TDataGridColumn('cnae', 'CNAE', 'left','10%');
+        $col_cnae_ordem  = new TDataGridColumn('cnae_ordem', 'CNAE Ordem', 'left','80%');
+        
+        $this->datagrid->addColumn($col_cnpj);
+        $this->datagrid->addColumn($col_cnae);
+        $this->datagrid->addColumn($col_cnae_ordem);
+
+        // create the datagrid model
+        $this->datagrid->createModel();
+
+        // creates the page navigation
+        $this->pageNavigation = new TPageNavigation;
+        $this->pageNavigation->setAction(new TAction(array($this, 'onReload')));
+
+        // creates the page structure using a table
         $vbox = new TVBox;
         $vbox->style = 'width: 100%';
         $vbox->add(new TXMLBreadCrumb('menu.xml', __CLASS__));
         $vbox->add($this->form);
+        $vbox->add(TPanelGroup::pack('', $this->datagrid, $this->pageNavigation));
+        
+        // add the table inside the page
         parent::add($vbox);
+    }
+
+    /**
+     * Clear filters
+     */
+    function clear()
+    {
+        $this->clearFilters();
+        $this->onReload();
     }
 }
