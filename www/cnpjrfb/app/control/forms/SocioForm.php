ad4fad803c1f47cfd057638f720187847f4c4ad8
Bjverde <bjverde@yahoo.com.br>
2020-01-29 22:33:45 -0300

    :hammer: ações

--- www/cnpjrfb/app/control/forms/SocioForm.php
+++ www/cnpjrfb/app/control/forms/SocioForm.php
@@ -47,16 +47,52 @@ class SocioForm extends TPage
         // create the datagrid
         $this->datagrid = new BootstrapDatagridWrapper(new TDataGrid);
         $this->datagrid->width = '100%';
+        $this->datagrid->datatable = 'true'; // turn on Datatables
         
         // add the columns
-        $col_cnpj        = new TDataGridColumn('cnpj', 'CNPJ Empresa', 'right','10%');
-        $col_tipo_socio  = new TDataGridColumn('tipo_socio', 'Tipo Sócio', 'left','10%');
-        $col_nome_socio  = new TDataGridColumn('nome_socio', 'Sócio', 'left','80%');
+        $col_cnpj        = new TDataGridColumn('cnpj', 'CNPJ Empresa', 'right');
+        $col_tipo_socio  = new TDataGridColumn('tipo_socio', 'Tipo Sócio', 'left');
+        $col_nome_socio  = new TDataGridColumn('nome_socio', 'Sócio', 'left');
         
         $this->datagrid->addColumn($col_cnpj);
         $this->datagrid->addColumn($col_tipo_socio);
         $this->datagrid->addColumn($col_nome_socio);
+        $this->datagrid->addColumn( new TDataGridColumn('cod_qualificacao','cod_qualificacao','left') );
+        $this->datagrid->addColumn( new TDataGridColumn('perc_capital','perc_capital','left') );
+        $this->datagrid->addColumn( new TDataGridColumn('data_entrada','data_entrada','left') );
+        $this->datagrid->addColumn( new TDataGridColumn('cod_pais_ext','cod_pais_ext','left') );
+        $this->datagrid->addColumn( new TDataGridColumn('nome_pais_ext','nome_pais_ext','left') );
+        $this->datagrid->addColumn( new TDataGridColumn('cpf_repres','cpf_repres','left') );
+        $this->datagrid->addColumn( new TDataGridColumn('nome_repres','nome_repres','left') );
+        $this->datagrid->addColumn( new TDataGridColumn('cod_qualif_repres','cod_qualif_repres','left') );
         
+
+        // creates two datagrid actions
+        $action1 = new TDataGridAction([$this, 'onView'],     ['key' => '{cnpj}']  );
+        $action2 = new TDataGridAction([$this, 'onDelete'],   ['key' => '{cnpj}' ] );
+        $action3 = new TDataGridAction([$this, 'onViewCity'], ['key' => '{cnpj}' ] );
+        
+        $action1->setLabel('View name');
+        $action1->setImage('fa:search #7C93CF');
+        
+        $action2->setLabel('Try to delete');
+        $action2->setImage('far:trash-alt red');
+        
+        $action3->setLabel('View city');
+        $action3->setImage('far:hand-pointer green');
+        
+        $action_group = new TDataGridActionGroup('Actions ', 'fa:th');
+        
+        $action_group->addHeader('Available Options');
+        $action_group->addAction($action1);
+        $action_group->addAction($action2);
+        $action_group->addSeparator();
+        $action_group->addHeader('Another Options');
+        $action_group->addAction($action3);
+        
+        // add the actions to the datagrid
+        $this->datagrid->addActionGroup($action_group);
+
         // create the datagrid model
         $this->datagrid->createModel();
 
@@ -73,6 +109,24 @@ class SocioForm extends TPage
         
         // add the table inside the page
         parent::add($vbox);
+    }
+
+    function onView()
+    {
+        $this->clearFilters();
+        $this->onReload();
+    }
+
+    function onDelete()
+    {
+        $this->clearFilters();
+        $this->onReload();
+    }
+
+    function onViewCity()
+    {
+        $this->clearFilters();
+        $this->onReload();
     }
 
     /**
