34856a25928cb7b407b41da64f392f1fe169b21f
Bjverde <bjverde@yahoo.com.br>
2020-02-09 02:46:24 -0300

    :bug: listar empresas

--- www/cnpjrfb/app/control/forms/SocioViewForm.php
+++ www/cnpjrfb/app/control/forms/SocioViewForm.php
@@ -45,10 +45,9 @@ class SocioViewForm extends TPage
             $nome_socio = $param['nome_socio'];
 
             $socioController = new SocioController();
-            $socio = $socioController->selectBySocio($cnpj_cpf_socio,$nome_socio);
-            var_dump($socio);
-            $this->form->setData($socio);
-            //$this->showGridSocios($empresa->getSocios());
+            $listSocio = $socioController->selectBySocio($cnpj_cpf_socio,$nome_socio);
+            $this->form->setData($listSocio);
+            $this->showGridEmpresa($listSocio);
         }
         catch(Exception $e)
         {
@@ -59,7 +58,25 @@ class SocioViewForm extends TPage
     public function showGridEmpresa($listSocio){
         $empresaController = new EmpresaController();
         $listEmpresa = $empresaController->selectBySocio($listSocio);
-        $panel = $empresaController->getGridEmpresa($listEmpresa);
+        $panel = $this->getGridEmpresa($listEmpresa);
         parent::add($panel);
+    }
+
+    function getGridEmpresa(array $listEmpresa){
+        // create the datagrid
+        $grid = new BootstrapDatagridWrapper(new TDataGrid);
+        $grid->width = '100%';    
+        $grid->addColumn( new TDataGridColumn('cnpj','CNPJ','left') );
+        $grid->addColumn( new TDataGridColumn('razao_social','Razão Social','left') );
+        $grid->addColumn( new TDataGridColumn('nome_fantasia','Nome Fantasia','left') );
+
+        $action1 = new TDataGridAction(['EmpresaViewForm', 'onView'],  ['key' => '{cnpj}'], ['register_state' => 'false']  );
+        $grid->addAction($action1, 'Detalhar Empresa', 'fa:building #7C93CF');
+
+        $grid->createModel();
+        $grid->addItems($listEmpresa);
+        $panel = TPanelGroup::pack('Lista de Empresas', $grid);
+        
+        return $panel;
     }
 }
