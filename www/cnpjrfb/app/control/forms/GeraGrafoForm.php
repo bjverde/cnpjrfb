c62453ef4e708115337f190e2e5e5818be1e66a8
Bjverde <bjverde@yahoo.com.br>
2020-02-23 02:13:33 -0300

    :hammer: gerador grafico quase funcionando

--- www/cnpjrfb/app/control/forms/GeraGrafoForm.php
+++ www/cnpjrfb/app/control/forms/GeraGrafoForm.php
@@ -36,19 +36,28 @@ class GeraGrafoForm extends TPage
 
     public function gerarGrafo($param){
         var_dump($param);
-        $path = 'app/cnpj_full/CNPJ-full/';
         if( empty($param['nome_socio']) ){
-            //$command = 'python3 consulta.py cnpj 00000000000191 graficos --gexf --viz';
-            $command = 'ls -l';
+            $command = 'python3 consulta.py cnpj 00000000000191 graficos --gexf --viz';
+            //$command = 'ls -l';
         }else{
-            //$command = 'python3 consulta.py cnpj 00000000000191 graficos --gexf --viz';
-            $command = 'ls -l';
+            $command = 'python3 consulta.py cnpj 00000000000191 graficos --gexf --viz';
+            //$command = 'ls -l';
         }
-        //$result = exec($path.$command, $output);
-        $result = exec($command, $output);
+        if (! defined ( 'DS' )) {
+            define ( 'DS', DIRECTORY_SEPARATOR );
+        }
+        $path = dirname ( __FILE__ );
+        $path = $path.DS.'..'.DS.'..'.DS.'cnpj_full'.DS.'CNPJ-full'.DS;
+        $command = 'cd '.$path.';'.$command.' 2>&1';
+        //$command = 'cd '.$path.';python3 consulta.py 2>&1';
+        //$command = 'cd '.$path.';ls -l';
+        var_dump($command);
+        $result01 = exec($command, $output, $result);
+        //$result = exec($command, $output);
         echo('<pre>');
         var_dump($output);
         var_dump($result);
+        var_dump($result01);
         echo('</pre>');
     }
 }
