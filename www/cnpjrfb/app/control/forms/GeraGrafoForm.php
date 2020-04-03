08bb1da6b90cf7a6252c35850841248adb260c0e
Bjverde <bjverde@yahoo.com.br>
2020-04-03 12:10:57 -0300

    :hammer: mostrando grafico

--- www/cnpjrfb/app/control/forms/GeraGrafoForm.php
+++ www/cnpjrfb/app/control/forms/GeraGrafoForm.php
@@ -34,35 +34,18 @@ class GeraGrafoForm extends TPage
         parent::add($vbox);
     }
 
-    /***
-     * 1 - sudo docker-compose exec apache_php bash
-     * 2 - chown -R www-data:www-data cnpjrfb/app/cnpj_full/graficos
-     */
     public function gerarGrafo($param){
-        Util::debug($param,'Param');
-        if( empty($param['nome_socio']) ){
-            $cnpj = $param['cnpj'];
-            $command = 'python3 consulta.py cnpj 00000000000191 graficos/00000000000191 --viz';
-            //$command = 'ls -l';
-        }else{
-            $nome_socio = $param['nome_socio'];
-            $cpf = null;
-            $command = 'python3 consulta.py nome_socio '.$nome_socio. ' graficos/00000000000191 --viz';            
-            //$command = 'ls -l';
-        }
-        if (! defined ( 'DS' )) {
-            define ( 'DS', DIRECTORY_SEPARATOR );
-        }
-        $path = dirname ( __FILE__ );
-        $path = $path.DS.'..'.DS.'..'.DS.'cnpj_full'.DS.'CNPJ-full'.DS;
-        $command = 'cd '.$path.';'.$command.' 2>&1';
-        //$command = 'cd '.$path.';python3 consulta.py 2>&1';
-        //$command = 'cd '.$path.';ls -l > t.txt'.' 2>&1';
-        $result01 = exec($command, $output, $result);
-        //$result = exec($command, $output);
-        Util::debug($command,'Command');
-        Util::debug($output,'Output');
-        Util::debug($result,'Result');
-        Util::debug($result01,'Result01');
+        $result = GeraGrafoController::executa($param);
+        parent::__construct();
+        
+        $iframe = new TElement('iframe');
+        $iframe->id = "iframe_external";
+        $iframe->src = "app/cnpj_full/CNPJ-full/graficos/grafo.html";
+        $iframe->frameborder = "0";
+        $iframe->scrolling = "yes";
+        $iframe->width = "100%";
+        $iframe->height = "700px";
+        
+        parent::add($iframe);
     }
 }
