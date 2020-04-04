8eb7032cbe6ae3c1fa0fd39d948b153746cd2777
Bjverde <bjverde@yahoo.com.br>
2020-04-04 01:40:18 -0300

    :hammer: alterando layout

--- www/cnpjrfb/app/lib/menu/AdiantiMenuBuilder.php
+++ www/cnpjrfb/app/lib/menu/AdiantiMenuBuilder.php
@@ -14,8 +14,10 @@ class AdiantiMenuBuilder
                 $menu->show();
                 $menu_string = ob_get_clean();
                 return $menu_string;
-                break;
+            break;
+            case 'theme3_v3':
             case 'theme3_v4':
+            case 'theme3_v5':
                     ob_start();
                     $xml = new SimpleXMLElement(file_get_contents($file));
                     $menu = new TMenu($xml, null, 1, 'treeview-menu', 'treeview', '');
