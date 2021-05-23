580060b17f12515826239813798a4dab06a8dda4
reinaldo.junior <reinaldo.junior@mpdft.mp.br>
2021-05-22 23:12:05 -0300

    :bug: #13

--- www/cnpjrfb/app/control/controllers/TipoEmpresaSituacao.php
+++ www/cnpjrfb/app/control/controllers/TipoEmpresaSituacao.php
@@ -5,20 +5,22 @@ class TipoEmpresaSituacao
     {
         $list = self::getList();
         $idQtd = strlen($id);
+        /*
         if($idQtd==1){
             $id = '0'.$id;
         }
+        */
         return $list[$id];
     }
 
     public static function getList()
     {
         $list = array(
-             '01'=>'NULA'
-            ,'02'=>'ATIVA'
-            ,'03'=>'SUSPENSA'
-            ,'04'=>'INAPTA'
-            ,'08'=>'BAIXADA'
+             '1'=>'NULA'
+            ,'2'=>'ATIVA'
+            ,'3'=>'SUSPENSA'
+            ,'4'=>'INAPTA'
+            ,'8'=>'BAIXADA'
         );
         return $list;
     }
