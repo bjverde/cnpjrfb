c7a9675e8f88c88ba1d5651acf6b8558277aa830
reinaldo.junior <reinaldo.junior@mpdft.mp.br>
2021-05-22 23:12:37 -0300

    :bug: #13

--- www/cnpjrfb/app/control/controllers/TipoEmpresaSituacao.php
+++ www/cnpjrfb/app/control/controllers/TipoEmpresaSituacao.php
@@ -4,12 +4,6 @@ class TipoEmpresaSituacao
     public static function getByid($id)
     {
         $list = self::getList();
-        $idQtd = strlen($id);
-        /*
-        if($idQtd==1){
-            $id = '0'.$id;
-        }
-        */
         return $list[$id];
     }
 
