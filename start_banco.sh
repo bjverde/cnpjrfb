0d3fc0294879ddd09df6039c6f74ff8cca00c10e
bjverde <bjverde@yahoo.com.br>
2021-10-30 14:50:40 -0300

    :hammer: #23 arquivo env

--- start_banco.sh
+++ start_banco.sh
@@ -20,14 +20,25 @@ cd /var/opt
 git clone https://github.com/aphonsoar/Receita_Federal_do_Brasil_-_Dados_Publicos_CNPJ.git aphonsoar
 
 
-echo -e "${LGREEN} Etapa 2/4 - Python install requirements ${NC}"
+echo -e "${LGREEN} Etapa 2/5 - Python install requirements ${NC}"
 cd /var/opt/aphonsoar
 python3.8 -m pip install --upgrade pip
 pip install python-dotenv
 pip install -r requirements.txt
 
-echo -e "${LGREEN} Etapa 3/4 - Python install requirements ${NC}"
-cd /var/opt/aphonsoar/code;python3.8 ETL_coletar_dados_e_gravar_BD.py
+echo -e "${LGREEN} Etapa 3/5 - Criando ENV ${NC}"
+echo "# Config Files" >> .env
+echo "OUTPUT_FILES_PATH=/var/opt/dados_receita/output_files" >> .env
+echo "EXTRACTED_FILES_PATH=/var/opt/dados_receita/extracted_files" >> .env
+echo "# Config Bando" >> .env
+echo "DB_HOST=localhost" >> .env
+echo "DB_PORT=5432" >> .env
+echo "DB_USER=postgres" >> .env
+echo "DB_PASSWORD=postgres" >> .env
+echo "DB_NAME=Dados_RFB" >> .env
+
+echo -e "${LGREEN} Etapa 4/5 - Executa ETL e criar banco ${NC}"
+python3.8 /var/opt/aphonsoar/code/ETL_coletar_dados_e_gravar_BD.py
 
 echo ''
 echo -e "${YBLUE} Alteração feita em $DESTINO ${NC}"
