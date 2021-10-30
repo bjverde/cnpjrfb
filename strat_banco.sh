c24b4b665302e2ecc54046ec0df3e5ecb72c13f2
bjverde <bjverde@yahoo.com.br>
2021-10-29 22:03:16 -0300

    :hammer: #23 alterando script

--- strat_banco.sh
+++ strat_banco.sh
@@ -15,14 +15,19 @@ echo ''
 echo -e "${YBLUE} Script START BANCO PostgreSQL ${NC}"
 echo ''
 
-echo -e "${LGREEN} Etapa 1/3 - Clonando Projeto Dados CNPJ do  aphonsoar ${NC}"
+echo -e "${LGREEN} Etapa 1/4 - Clonando Projeto Dados CNPJ do  aphonsoar ${NC}"
 cd /var/opt
 git clone https://github.com/aphonsoar/Receita_Federal_do_Brasil_-_Dados_Publicos_CNPJ.git aphonsoar
 
 
-echo -e "${LGREEN} Etapa 2/3 - Python install requirements ${NC}"
+echo -e "${LGREEN} Etapa 2/4 - Python install requirements ${NC}"
 cd /var/opt/aphonsoar
+python3 -m pip install --upgrade pip
 pip install -r requirements.txt
+
+echo -e "${LGREEN} Etapa 3/4 - Python install requirements ${NC}"
+cd /var/opt/aphonsoar
+python3 pip ETL_coletar_dados_e_gravar_BD.py
 
 echo ''
 echo -e "${YBLUE} Alteração feita em $DESTINO ${NC}"
