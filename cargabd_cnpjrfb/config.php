7fd4c82cd93a0fbd4ed8027cd03b43284d9eeb06
bjverde <bjverde@yahoo.com.br>
2021-11-14 23:37:21 -0300

    :hammer: #26 primeira versão

---
+++ cargabd_cnpjrfb/config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * db_drive = Drive do PDO  : mysql, sqlite, pgsql
+ * db_host = endereço do servido de banco de dados
+ * db_port = porta do servidor , null ou branco será considerado a porta default
+ */
+return [
+     "db_drive" => "mysql"
+    ,"db_host" => "127.0.0.1"
+    ,"db_port" => "3306"
+    ,"db_name" => "avalicao_risco"
+    ,"db_user" => "avalicao_risco"
+    ,"db_password" => "123456"
+    ,"EXTRACTED_FILES_PATH"=>"/var/opt/dados_receita/extracted_files"
+];
---
+++ cargabd_cnpjrfb/index.php
@@ -0,0 +1,7 @@
+<?php
+if (version_compare(PHP_VERSION, '7.4.0') == -1)
+{
+    die ('ERRO: The minimum version required for PHP is 7.4.0');
+}
+
+echo "fldajfkl";
