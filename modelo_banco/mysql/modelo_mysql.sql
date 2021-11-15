2ee6b85a7f28d1aad0e1a9d6a83f2501ef6b7b7a
bjverde <bjverde@yahoo.com.br>
2021-11-15 01:05:11 -0300

    :hammer: #26 melhoria script

--- modelo_banco/mysql/modelo_mysql.sql
+++ modelo_banco/mysql/modelo_mysql.sql
@@ -19,11 +19,11 @@ CREATE SCHEMA IF NOT EXISTS `dados_rfb` DEFAULT CHARACTER SET utf8 COLLATE utf8_
 USE `dados_rfb` ;
 
 -- -----------------------------------------------------
--- Table `dados_rfb`.`cnae`
+-- Table `cnae`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `dados_rfb`.`cnae` ;
+DROP TABLE IF EXISTS `cnae` ;
 
-CREATE TABLE IF NOT EXISTS `dados_rfb`.`cnae` (
+CREATE TABLE IF NOT EXISTS `cnae` (
   `codigo` INT NOT NULL,
   `descricao` VARCHAR(1000) NOT NULL,
   PRIMARY KEY (`codigo`))
@@ -31,22 +31,22 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `dados_rfb`.`simples`
+-- Table `simples`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `dados_rfb`.`simples` ;
+DROP TABLE IF EXISTS `simples` ;
 
-CREATE TABLE IF NOT EXISTS `dados_rfb`.`simples` (
+CREATE TABLE IF NOT EXISTS `simples` (
   `cnpj_basico` INT NOT NULL,
   PRIMARY KEY (`cnpj_basico`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `dados_rfb`.`quals`
+-- Table `quals`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `dados_rfb`.`quals` ;
+DROP TABLE IF EXISTS `quals` ;
 
-CREATE TABLE IF NOT EXISTS `dados_rfb`.`quals` (
+CREATE TABLE IF NOT EXISTS `quals` (
   `codigo` INT NOT NULL,
   `descricao` VARCHAR(1000) NOT NULL,
   PRIMARY KEY (`codigo`))
@@ -56,3 +56,9 @@ ENGINE = InnoDB;
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+
+
+DROP USER IF EXISTS 'dados_rfb'@'%';
+CREATE USER 'dados_rfb'@'%' IDENTIFIED BY '123456';
+GRANT DELETE,EXECUTE,INSERT,SELECT,UPDATE ON dados_rfb.* TO 'dados_rfb'@'%';
