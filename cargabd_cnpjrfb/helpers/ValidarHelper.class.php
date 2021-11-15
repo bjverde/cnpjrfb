c5e1e2d83f4bde755772b866b5c3d1cdfe1e44d3
bjverde <bjverde@yahoo.com.br>
2021-11-15 16:05:55 -0300

    :hammer: #26 nova validação

--- cargabd_cnpjrfb/helpers/ValidarHelper.class.php
+++ cargabd_cnpjrfb/helpers/ValidarHelper.class.php
@@ -1,10 +1,18 @@
 <?php
 
 class ValidarHelper
-{        
+{      
+    
+    public static function validarExtensaoCarregada($extensao=null)
+    {
+        if (!extension_loaded($extensao)) {
+            throw new InvalidArgumentException('ERRO: extensão do PHP '.$extensao.' não está instalada!');
+        }
+    }
     public static function InformacoesIniciais()
     {
         try{
+            self::validarExtensaoCarregada('mbstring');
             TPDOConnection::validarDrive();
             TPDOConnection::validarDBMS();
             ConfigHelper::getExtractedFilesPath();
