c7d1137f6262a2685532018abd804d8f55274fc3
bjverde <bjverde@yahoo.com.br>
2021-11-15 00:42:02 -0300

    :bug: #26 correções

--- cargabd_cnpjrfb/index.php
+++ cargabd_cnpjrfb/index.php
@@ -1,6 +1,5 @@
 <?php
-if (version_compare(PHP_VERSION, '7.4.0') == -1)
-{
+if (version_compare(PHP_VERSION, '7.4.0') == -1) {
     die ('ERRO: The minimum version required for PHP is 7.4.0');
 }
 
@@ -8,9 +7,8 @@ define( 'DS', DIRECTORY_SEPARATOR );
 define('ROOT_PATH'     , __DIR__);           // Caminho completo no SO
 define('ROOT_FOLDER'   , basename(__DIR__)); //Folder root name
 
+require_once 'helpers/autoload_formdin_helper.php';
 require_once 'controllers/autoload_cargabd_cnpjrfb.php';
-//require_once 'dao/autoload_cargabd_cnpjrfb_dao.php';
+require_once 'dao/autoload_cargabd_cnpjrfb_dao.php';
 
-
-
-Validar::InformacoesIniciais();
+ValidarHelper::InformacoesIniciais();
