a4da85eb325ecc5dcdd517d12ccb51673658b7e2
bjverde <bjverde@yahoo.com.br>
2021-11-15 01:01:39 -0300

    :bug: #26 correção config

--- cargabd_cnpjrfb/config.php
+++ cargabd_cnpjrfb/config.php
@@ -8,8 +8,8 @@ return [
      "db_drive" => "mysql"
     ,"db_host" => "127.0.0.1"
     ,"db_port" => "3306"
-    ,"db_name" => "avalicao_risco"
-    ,"db_user" => "avalicao_risco"
+    ,"db_name" => "dados_rfb"
+    ,"db_user" => "dados_rfb"
     ,"db_password" => "123456"
     ,"EXTRACTED_FILES_PATH"=>"/var/opt/dados_receita/extracted_files"
 ];
