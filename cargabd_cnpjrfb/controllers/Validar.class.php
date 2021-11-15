603640d7c7021455ace91618bdb42751a942f723
bjverde <bjverde@yahoo.com.br>
2021-11-15 00:06:26 -0300

    :hammer: #24 informações iniciais

---
+++ cargabd_cnpjrfb/controllers/Validar.class.php
@@ -0,0 +1,19 @@
+<?php
+
+class Validar
+{        
+    public static function InformacoesIniciais()
+    {
+        try{
+            self::drive();
+        }
+        catch (Exception $e) {
+            print($e->getMessage());
+        }
+    }
+
+    public static function drive()
+    {
+        throw new InvalidArgumentException('Teste');
+    }
+}
--- cargabd_cnpjrfb/index.php
+++ cargabd_cnpjrfb/index.php
@@ -4,4 +4,10 @@ if (version_compare(PHP_VERSION, '7.4.0') == -1)
     die ('ERRO: The minimum version required for PHP is 7.4.0');
 }
 
-echo "fldajfkl";
+require_once 'controllers/autoload_cargabd_cnpjrfb.php';
+require_once 'dao/autoload_cargabd_cnpjrfb_dao.php';
+
+define('ROOT_PATH'     , __DIR__);           // Caminho completo no SO
+define('ROOT_FOLDER'   , basename(__DIR__)); //Folder root name
+
+Validar::InformacoesIniciais();
