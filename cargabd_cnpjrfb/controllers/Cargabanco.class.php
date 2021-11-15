e7b1ebd3fe95653e2fa480beee96c550bf3e840d
bjverde <bjverde@yahoo.com.br>
2021-11-15 14:37:49 -0300

    :hammer: #26 formatando script cli mode

--- cargabd_cnpjrfb/controllers/Cargabanco.class.php
+++ cargabd_cnpjrfb/controllers/Cargabanco.class.php
@@ -3,25 +3,39 @@
 class Cargabanco
 {    
     private $cnaeDAO = null;
+    private $is_cli  = false;
 
     public function __construct()
     {
         $tpdo = New TPDOConnection();
         $tpdo::connect();
         $this->cnaeDAO = new CnaeDAO($tpdo);
+        
+        if (php_sapi_name() == "cli") {
+            $this->is_cli = true;
+        }
     }
-
     public function executar(){
         try{
+            $this->quebraLinha();
+            $this->quebraLinha();
             echo 'Esse procedimento vai apagar todo o banco e carregar com os dados dos arquivos CSV';
-            echo '<br>';
+            $this->quebraLinha();
+            $this->quebraLinha();
             $this->truncateDados();
+            
         }
         catch (Exception $e) {
             print($e->getMessage());
         }
     }
-
+    public function quebraLinha(){
+        $string = '<br>';
+        if ( $this->is_cli ) {
+            $string = PHP_EOL;
+        }
+        echo $string;
+    }
     public function truncateDados(){
         $this->truncateTabela($this->cnaeDAO);
     }
@@ -32,5 +46,6 @@ class Cargabanco
         }else{
             echo 'Tabela: '.$classDao->getTabelaName().' com '.$qtd.' registros apagados';
         }
+        $this->quebraLinha();
     }
 }
