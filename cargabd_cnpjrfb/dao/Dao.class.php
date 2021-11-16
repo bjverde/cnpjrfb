c4472a31582caada172660cf5c4389f9c9c9f8c1
bjverde <bjverde@yahoo.com.br>
2021-11-16 13:32:18 -0300

    :bug: #26 correção do problema da chave

--- cargabd_cnpjrfb/dao/Dao.class.php
+++ cargabd_cnpjrfb/dao/Dao.class.php
@@ -46,7 +46,15 @@ class Dao
     //--------------------------------------------------------------------------------
     public function truncate()
     {
-        $sql = 'TRUNCATE '.$this->getTabelaName();
+        $sql = 'TRUNCATE '.$this->getTabelaName().';';
+        $tpdo = $this->tpdo;
+        if( $tpdo->getDBMS()==TPDOConnection::DBMS_MYSQL ){
+            //https://pt.stackoverflow.com/questions/256158/existe-algum-risco-em-usar-set-foreign-key-checks-0
+            $sql = 'SET FOREIGN_KEY_CHECKS=0;'.$sql.';SET FOREIGN_KEY_CHECKS=1;';
+        }elseif($tpdo->getDBMS()==TPDOConnection::DBMS_POSTGRES){
+            //https://stackoverflow.com/questions/38112379/disable-postgresql-foreign-key-checks-for-migrations
+            $sql = "SET session_replication_role = 'replica';".$sql.";SET session_replication_role = 'origin';";
+        }        
         $result = $this->tpdo->executeSql($sql);
         return $result;
     }
