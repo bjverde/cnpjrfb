6aa45468e80bf3b5c00977f76e4565419e93a1c8
bjverde <bjverde@yahoo.com.br>
2021-11-21 18:09:39 -0300

    :bug: #26 melhorando carga

--- www/cargabd/controllers/UploadCsv.class.php
+++ www/cargabd/controllers/UploadCsv.class.php
@@ -16,13 +16,17 @@ class UploadCsv {
         $separador = ';';
         $file = fopen($this->arquivoCsv, 'r');
         while ( ($line = fgets ($file)) !== false ){
+        //for ($i = 1; $i <= 1000; $i++) {
+        //$line = fgets ($file);
             //Limpando a linha
             $line = StringHelper::str2utf8($line);
             $line = preg_replace('/["]/', '', $line);
             $line = strtr($line, chr(13), chr(32));// For CR
             $line = strtr($line, chr(10), chr(32));// For LF
-            $line = explode($separador, $line);
-            $this->dao->insert( $line );
+            if( !empty($line) ){
+                $line = explode($separador, $line);
+                $this->dao->insert( $line );
+            }
             $numRegistros = $numRegistros+1;;
         }
         fclose($file);
