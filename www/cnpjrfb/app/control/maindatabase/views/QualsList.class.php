64d2c0911d0b094211a6ac7ecb745f4d7abbcd4e
bjverde <bjverde@yahoo.com.br>
2021-12-06 17:47:55 -0300

    :hammer: #31 novas telas

---
+++ www/cnpjrfb/app/control/maindatabase/views/QualsList.class.php
@@ -0,0 +1,434 @@
+<?php
+
+class QualsList extends TPage
+{
+    private $form; // form
+    private $datagrid; // listing
+    private $pageNavigation;
+    private $loaded;
+    private $filter_criteria;
+    private static $database = 'maindatabase';
+    private static $activeRecord = 'Quals';
+    private static $primaryKey = 'codigo';
+    private static $formName = 'form_QualsList';
+    private $showMethods = ['onReload', 'onSearch', 'onRefresh', 'onClearFilters'];
+    private $limit = 20;
+
+    /**
+     * Class constructor
+     * Creates the page, the form and the listing
+     */
+    public function __construct($param = null)
+    {
+        parent::__construct();
+
+        if(!empty($param['target_container']))
+        {
+            $this->adianti_target_container = $param['target_container'];
+        }
+
+        // creates the form
+        $this->form = new BootstrapFormBuilder(self::$formName);
+
+        // define the form title
+        $this->form->setFormTitle("Qualificação");
+        $this->limit = 20;
+
+        $codigo = new TEntry('codigo');
+        $descricao = new TEntry('descricao');
+
+
+        $codigo->setSize(100);
+        $descricao->setSize('100%');
+
+        $row1 = $this->form->addFields([new TLabel("Codigo:", null, '14px', null)],[$codigo],[new TLabel("Descricao:", null, '14px', null)],[$descricao]);
+
+        // keep the form filled during navigation with session data
+        $this->form->setData( TSession::getValue(__CLASS__.'_filter_data') );
+
+        $btn_onsearch = $this->form->addAction("Buscar", new TAction([$this, 'onSearch']), 'fas:search #ffffff');
+        $this->btn_onsearch = $btn_onsearch;
+        $btn_onsearch->addStyleClass('btn-primary'); 
+
+        // creates a Datagrid
+        $this->datagrid = new TDataGrid;
+        $this->datagrid->disableHtmlConversion();
+
+        $this->datagrid_form = new TForm('datagrid_'.self::$formName);
+        $this->datagrid_form->onsubmit = 'return false';
+
+        $this->datagrid = new BootstrapDatagridWrapper($this->datagrid);
+        $this->filter_criteria = new TCriteria;
+
+        $this->datagrid->style = 'width: 100%';
+        $this->datagrid->setHeight(320);
+
+        $column_codigo = new TDataGridColumn('codigo', "Codigo", 'center' , '70px');
+        $column_descricao = new TDataGridColumn('descricao', "Descricao", 'left');
+
+        $order_codigo = new TAction(array($this, 'onReload'));
+        $order_codigo->setParameter('order', 'codigo');
+        $column_codigo->setAction($order_codigo);
+
+        $this->datagrid->addColumn($column_codigo);
+        $this->datagrid->addColumn($column_descricao);
+
+
+        // create the datagrid model
+        $this->datagrid->createModel();
+
+        // creates the page navigation
+        $this->pageNavigation = new TPageNavigation;
+        $this->pageNavigation->enableCounters();
+        $this->pageNavigation->setAction(new TAction(array($this, 'onReload')));
+        $this->pageNavigation->setWidth($this->datagrid->getWidth());
+
+        $panel = new TPanelGroup("");
+        $panel->datagrid = 'datagrid-container';
+        $this->datagridPanel = $panel;
+        $this->datagrid_form->add($this->datagrid);
+        $panel->add($this->datagrid_form);
+
+        $panel->getBody()->class .= ' table-responsive';
+
+        $panel->addFooter($this->pageNavigation);
+
+        $headerActions = new TElement('div');
+        $headerActions->class = ' datagrid-header-actions ';
+        $headerActions->style = 'background-color:#fff; justify-content: space-between;';
+
+        $head_left_actions = new TElement('div');
+        $head_left_actions->class = ' datagrid-header-actions-left-actions ';
+
+        $head_right_actions = new TElement('div');
+        $head_right_actions->class = ' datagrid-header-actions-left-actions ';
+
+        $headerActions->add($head_left_actions);
+        $headerActions->add($head_right_actions);
+
+        $panel->getBody()->insert(0, $headerActions);
+
+        $dropdown_button_exportar = new TDropDown("Exportar", 'fas:file-export #2d3436');
+        $dropdown_button_exportar->setPullSide('right');
+        $dropdown_button_exportar->setButtonClass('btn btn-default waves-effect dropdown-toggle');
+        $dropdown_button_exportar->addPostAction( "CSV", new TAction(['QualsList', 'onExportCsv'],['static' => 1]), 'datagrid_'.self::$formName, 'fas:table #00b894' );
+        $dropdown_button_exportar->addPostAction( "PDF", new TAction(['QualsList', 'onExportPdf'],['static' => 1]), 'datagrid_'.self::$formName, 'far:file-pdf #e74c3c' );
+        $dropdown_button_exportar->addPostAction( "XML", new TAction(['QualsList', 'onExportXml'],['static' => 1]), 'datagrid_'.self::$formName, 'far:file-code #95a5a6' );
+
+        $head_right_actions->add($dropdown_button_exportar);
+
+        // vertical box container
+        $container = new TVBox;
+        $container->style = 'width: 100%';
+        if(empty($param['target_container']))
+        {
+            $container->add(TBreadCrumb::create(["Tabelas Apoio","Qualificação"]));
+        }
+        $container->add($this->form);
+        $container->add($panel);
+
+        parent::add($container);
+
+    }
+
+    public function onExportCsv($param = null) 
+    {
+        try
+        {
+            $output = 'app/output/'.uniqid().'.csv';
+
+            if ( (!file_exists($output) && is_writable(dirname($output))) OR is_writable($output))
+            {
+                $this->limit = 0;
+                $objects = $this->onReload();
+
+                if ($objects)
+                {
+                    $handler = fopen($output, 'w');
+                    TTransaction::open(self::$database);
+
+                    foreach ($objects as $object)
+                    {
+                        $row = [];
+                        foreach ($this->datagrid->getColumns() as $column)
+                        {
+                            $column_name = $column->getName();
+
+                            if (isset($object->$column_name))
+                            {
+                                $row[] = is_scalar($object->$column_name) ? $object->$column_name : '';
+                            }
+                            else if (method_exists($object, 'render'))
+                            {
+                                $column_name = (strpos($column_name, '{') === FALSE) ? ( '{' . $column_name . '}') : $column_name;
+                                $row[] = $object->render($column_name);
+                            }
+                        }
+
+                        fputcsv($handler, $row);
+                    }
+
+                    fclose($handler);
+                    TTransaction::close();
+                }
+                else
+                {
+                    throw new Exception(_t('No records found'));
+                }
+
+                TPage::openFile($output);
+            }
+            else
+            {
+                throw new Exception(_t('Permission denied') . ': ' . $output);
+            }
+        }
+        catch (Exception $e) // in case of exception
+        {
+            new TMessage('error', $e->getMessage()); // shows the exception error message
+        }
+    }
+
+    public function onExportPdf($param = null) 
+    {
+        try
+        {
+            $output = 'app/output/'.uniqid().'.pdf';
+
+            if ( (!file_exists($output) && is_writable(dirname($output))) OR is_writable($output))
+            {
+                $this->limit = 0;
+                $this->datagrid->prepareForPrinting();
+                $this->onReload();
+
+                $html = clone $this->datagrid;
+                $contents = file_get_contents('app/resources/styles-print.html') . $html->getContents();
+
+                $dompdf = new \Dompdf\Dompdf;
+                $dompdf->loadHtml($contents);
+                $dompdf->setPaper('A4', 'portrait');
+                $dompdf->render();
+
+                file_put_contents($output, $dompdf->output());
+
+                $window = TWindow::create('PDF', 0.8, 0.8);
+                $object = new TElement('object');
+                $object->data  = $output;
+                $object->type  = 'application/pdf';
+                $object->style = "width: 100%; height:calc(100% - 10px)";
+
+                $window->add($object);
+                $window->show();
+            }
+            else
+            {
+                throw new Exception(_t('Permission denied') . ': ' . $output);
+            }
+        }
+        catch (Exception $e) // in case of exception
+        {
+            new TMessage('error', $e->getMessage()); // shows the exception error message
+        }
+    }
+
+    public function onExportXml($param = null) 
+    {
+        try
+        {
+            $output = 'app/output/'.uniqid().'.xml';
+
+            if ( (!file_exists($output) && is_writable(dirname($output))) OR is_writable($output))
+            {
+                $this->limit = 0;
+                $objects = $this->onReload();
+
+                if ($objects)
+                {
+                    TTransaction::open(self::$database);
+
+                    $dom = new DOMDocument('1.0', 'UTF-8');
+                    $dom->{'formatOutput'} = true;
+                    $dataset = $dom->appendChild( $dom->createElement('dataset') );
+
+                    foreach ($objects as $object)
+                    {
+                        $row = $dataset->appendChild( $dom->createElement( self::$activeRecord ) );
+
+                        foreach ($this->datagrid->getColumns() as $column)
+                        {
+                            $column_name = $column->getName();
+                            $column_name_raw = str_replace(['(','{','->', '-','>','}',')', ' '], ['','','_','','','','','_'], $column_name);
+
+                            if (isset($object->$column_name))
+                            {
+                                $value = is_scalar($object->$column_name) ? $object->$column_name : '';
+                                $row->appendChild($dom->createElement($column_name_raw, $value)); 
+                            }
+                            else if (method_exists($object, 'render'))
+                            {
+                                $column_name = (strpos($column_name, '{') === FALSE) ? ( '{' . $column_name . '}') : $column_name;
+                                $value = $object->render($column_name);
+                                $row->appendChild($dom->createElement($column_name_raw, $value));
+                            }
+                        }
+                    }
+
+                    $dom->save($output);
+
+                    TTransaction::close();
+                }
+                else
+                {
+                    throw new Exception(_t('No records found'));
+                }
+
+                TPage::openFile($output);
+            }
+            else
+            {
+                throw new Exception(_t('Permission denied') . ': ' . $output);
+            }
+        }
+        catch (Exception $e) // in case of exception
+        {
+            new TMessage('error', $e->getMessage()); // shows the exception error message
+            TTransaction::rollback(); // undo all pending operations
+        }
+    }
+
+    /**
+     * Register the filter in the session
+     */
+    public function onSearch($param = null)
+    {
+        $data = $this->form->getData();
+        $filters = [];
+
+        TSession::setValue(__CLASS__.'_filter_data', NULL);
+        TSession::setValue(__CLASS__.'_filters', NULL);
+
+        if (isset($data->codigo) AND ( (is_scalar($data->codigo) AND $data->codigo !== '') OR (is_array($data->codigo) AND (!empty($data->codigo)) )) )
+        {
+
+            $filters[] = new TFilter('codigo', '=', $data->codigo);// create the filter 
+        }
+
+        if (isset($data->descricao) AND ( (is_scalar($data->descricao) AND $data->descricao !== '') OR (is_array($data->descricao) AND (!empty($data->descricao)) )) )
+        {
+
+            $filters[] = new TFilter('descricao', 'like', "%{$data->descricao}%");// create the filter 
+        }
+
+        // fill the form with data again
+        $this->form->setData($data);
+
+        // keep the search data in the session
+        TSession::setValue(__CLASS__.'_filter_data', $data);
+        TSession::setValue(__CLASS__.'_filters', $filters);
+
+        $this->onReload(['offset' => 0, 'first_page' => 1]);
+    }
+
+    /**
+     * Load the datagrid with data
+     */
+    public function onReload($param = NULL)
+    {
+        try
+        {
+            // open a transaction with database 'cnpjrfb'
+            TTransaction::open(self::$database);
+
+            // creates a repository for Quals
+            $repository = new TRepository(self::$activeRecord);
+
+            $criteria = clone $this->filter_criteria;
+
+            if (empty($param['order']))
+            {
+                $param['order'] = 'codigo';    
+            }
+
+            if (empty($param['direction']))
+            {
+                $param['direction'] = 'desc';
+            }
+
+            $criteria->setProperties($param); // order, offset
+            $criteria->setProperty('limit', $this->limit);
+
+            if($filters = TSession::getValue(__CLASS__.'_filters'))
+            {
+                foreach ($filters as $filter) 
+                {
+                    $criteria->add($filter);       
+                }
+            }
+
+            // load the objects according to criteria
+            $objects = $repository->load($criteria, FALSE);
+
+            $this->datagrid->clear();
+            if ($objects)
+            {
+                // iterate the collection of active records
+                foreach ($objects as $object)
+                {
+
+                    $row = $this->datagrid->addItem($object);
+                    $row->id = "row_{$object->codigo}";
+
+                }
+            }
+
+            // reset the criteria for record count
+            $criteria->resetProperties();
+            $count= $repository->count($criteria);
+
+            $this->pageNavigation->setCount($count); // count of records
+            $this->pageNavigation->setProperties($param); // order, page
+            $this->pageNavigation->setLimit($this->limit); // limit
+
+            // close the transaction
+            TTransaction::close();
+            $this->loaded = true;
+
+            return $objects;
+        }
+        catch (Exception $e) // in case of exception
+        {
+            // shows the exception error message
+            new TMessage('error', $e->getMessage());
+            // undo all pending operations
+            TTransaction::rollback();
+        }
+    }
+
+    public function onShow($param = null)
+    {
+
+    }
+
+    /**
+     * method show()
+     * Shows the page
+     */
+    public function show()
+    {
+        // check if the datagrid is already loaded
+        if (!$this->loaded AND (!isset($_GET['method']) OR !(in_array($_GET['method'],  $this->showMethods))) )
+        {
+            if (func_num_args() > 0)
+            {
+                $this->onReload( func_get_arg(0) );
+            }
+            else
+            {
+                $this->onReload();
+            }
+        }
+        parent::show();
+    }
+
+}
+
