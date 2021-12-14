39c5b6ed521a3a05e3ba234a1dec3e01f7e5e54d
raphael.cavalcante <raphael.cavalcante@mpdft.mp.br>
2021-12-14 13:17:48 -0300

    adicionada a tablea estabelecimento

--- modelo_banco/mysql/modelo_sqlserver.sql
+++ modelo_banco/mysql/modelo_sqlserver.sql
@@ -163,67 +163,133 @@ EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Descrição do
 
 
 -- -----------------------------------------------------
--- Table 'estabelecimento'
+-- Table estabelecimento
 -- -----------------------------------------------------
 
-CREATE TABLE IF NOT EXISTS 'estabelecimento' (
-  'cnpj_basico' CHAR(8) NOT NULL COMMENT 'NÚMERO BASE DE INSCRIÇÃO NO CNPJ (OITO PRIMEIROS DÍGITOS DO CNPJ).',
-  'cnpj_ordem' CHAR(4) NOT NULL COMMENT 'NÚMERO DO ESTABELECIMENTO DE INSCRIÇÃO NO CNPJ (DO NONO ATÉ O DÉCIMO SEGUNDO DÍGITO DO CNPJ).',
-  'cnpj_dv' CHAR(2) NOT NULL COMMENT 'DÍGITO VERIFICADOR DO NÚMERO DE INSCRIÇÃO NO CNPJ (DOIS ÚLTIMOS DÍGITOS DO CNPJ).',
-  'identificador_matriz_filial' CHAR(1) NOT NULL COMMENT 'CÓDIGO DO IDENTIFICADOR MATRIZ/FILIAL:\n1 – MATRIZ\n2 – FILIAL',
-  'nome_fantasia' VARCHAR(1000) NULL COMMENT 'CORRESPONDE AO NOME FANTASIA',
-  'situacao_cadastral' CHAR(1) NOT NULL COMMENT 'CÓDIGO DA SITUAÇÃO CADASTRAL:\n01 – NULA\n2 – ATIVA\n3 – SUSPENSA\n4 – INAPTA\n08 – BAIXADA',
-  'data_situacao_cadastral' DATE NULL COMMENT 'DATA DO EVENTO DA SITUAÇÃO CADASTRAL',
-  'motivo_situacao_cadastral' INT NOT NULL COMMENT 'CÓDIGO DO MOTIVO DA SITUAÇÃO CADASTRAL',
-  'nome_cidade_exterior' VARCHAR(45) NULL COMMENT 'NOME DA CIDADE NO EXTERIOR',
-  'pais' INT NULL COMMENT 'CÓDIGO DO PAIS',
-  'data_inicio_atividade' DATETIME NULL COMMENT 'DATA DE INÍCIO DA ATIVIDADE',
-  'cnae_fiscal_principal' INT NOT NULL COMMENT 'CÓDIGO DA ATIVIDADE ECONÔMICA PRINCIPAL DO ESTABELECIMENTO',
-  'cnae_fiscal_secundaria' VARCHAR(1000) NULL COMMENT 'CÓDIGO DA(S) ATIVIDADE(S) ECONÔMICA(S) SECUNDÁRIA(S) DO ESTABELECIMENTO',
-  'tipo_logradouro' VARCHAR(500) NULL,
-  'logradouro' VARCHAR(1000) NULL COMMENT 'NOME DO LOGRADOURO ONDE SE LOCALIZA O ESTABELECIMENTO.',
-  'numero' VARCHAR(45) NULL COMMENT 'NÚMERO ONDE SE LOCALIZA O ESTABELECIMENTO. QUANDO NÃO HOUVER PREENCHIMENTO DO NÚMERO HAVERÁ ‘S/N’.',
-  'complemento' VARCHAR(100) NULL COMMENT 'COMPLEMENTO PARA O ENDEREÇO DE LOCALIZAÇÃO DO ESTABELECIMENTO',
-  'bairro' VARCHAR(45) NULL COMMENT 'BAIRRO ONDE SE LOCALIZA O ESTABELECIMENTO.',
-  'cep' VARCHAR(45) NULL COMMENT 'CÓDIGO DE ENDEREÇAMENTO POSTAL REFERENTE AO LOGRADOURO NO QUAL O ESTABELECIMENTO ESTA LOCALIZADO',
-  'uf' VARCHAR(45) NULL COMMENT 'SIGLA DA UNIDADE DA FEDERAÇÃO EM QUE SE ENCONTRA O ESTABELECIMENTO',
-  'municipio' INT NULL COMMENT 'CÓDIGO DO MUNICÍPIO DE JURISDIÇÃO ONDE SE ENCONTRA O ESTABELECIMENTO',
-  'ddd_1' VARCHAR(45) NULL,
-  'telefone_1' VARCHAR(45) NULL,
-  'ddd_2' VARCHAR(45) NULL,
-  'telefone_2' VARCHAR(45) NULL,
-  'ddd_fax' VARCHAR(45) NULL,
-  'fax' VARCHAR(45) NULL,
-  'correio_eletronico' VARCHAR(45) NULL,
-  'situacao_especial' VARCHAR(45) NULL,
-  'data_situacao_especial' DATE NULL,
-  PRIMARY KEY ('cnpj_basico'),
-  INDEX 'fk_estabelecimento_pais_idx' ('pais' ASC) VISIBLE,
-  INDEX 'fk_estabelecimento_munic1_idx' ('municipio' ASC) VISIBLE,
-  INDEX 'fk_estabelecimento_cnae1_idx' ('cnae_fiscal_principal' ASC) VISIBLE,
-  INDEX 'fk_estabelecimento_moti1_idx' ('motivo_situacao_cadastral' ASC) VISIBLE,
-  CONSTRAINT 'fk_estabelecimento_pais'
-    FOREIGN KEY ('pais')
-    REFERENCES 'dados_rfb'.'pais' ('codigo')
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT 'fk_estabelecimento_munic1'
-    FOREIGN KEY ('municipio')
-    REFERENCES 'dados_rfb'.'munic' ('codigo')
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT 'fk_estabelecimento_cnae1'
-    FOREIGN KEY ('cnae_fiscal_principal')
-    REFERENCES 'dados_rfb'.'cnae' ('codigo')
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT 'fk_estabelecimento_moti1'
-    FOREIGN KEY ('motivo_situacao_cadastral')
-    REFERENCES 'dados_rfb'.'moti' ('codigo')
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+CREATE TABLE estabelecimento (
+  cnpj_basico CHAR(8) NOT NULL,
+  cnpj_ordem CHAR(4) NOT NULL,
+  cnpj_dv CHAR(2) NOT NULL,
+  identificador_matriz_filial CHAR(1) NOT NULL,
+  nome_fantasia VARCHAR(1000) NULL,
+  situacao_cadastral CHAR(1) NOT NULL,
+  data_situacao_cadastral DATE NULL,
+  motivo_situacao_cadastral INT NOT NULL,
+  nome_cidade_exterior VARCHAR(45) NULL,
+  pais INT NULL,
+  data_inicio_atividade DATETIME NULL,
+  cnae_fiscal_principal INT NOT NULL,
+  cnae_fiscal_secundaria VARCHAR(1000) NULL,
+  tipo_logradouro VARCHAR(500) NULL,
+  logradouro VARCHAR(1000) NULL,
+  numero VARCHAR(45) NULL,
+  complemento VARCHAR(100) NULL,
+  bairro VARCHAR(45) NULL,
+  cep VARCHAR(45) NULL,
+  uf VARCHAR(45) NULL,
+  municipio INT NULL,
+  ddd_1 VARCHAR(45) NULL,
+  telefone_1 VARCHAR(45) NULL,
+  ddd_2 VARCHAR(45) NULL,
+  telefone_2 VARCHAR(45) NULL,
+  ddd_fax VARCHAR(45) NULL,
+  fax VARCHAR(45) NULL,
+  correio_eletronico VARCHAR(45) NULL,
+  situacao_especial VARCHAR(45) NULL,
+  data_situacao_especial DATE NULL,
+	CONSTRAINT [PK_CNPJ_BASICO] PRIMARY KEY CLUSTERED 
+		  (
+			cnpj_basico ASC
+		  )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+   )ON [PRIMARY]
 
+
+
+CREATE INDEX fk_estabelecimento_pais_idx ON estabelecimento (pais ASC);
+CREATE INDEX fk_estabelecimento_munic1_idx ON estabelecimento (municipio ASC);
+CREATE INDEX fk_estabelecimento_cnae1_idx ON estabelecimento (cnae_fiscal_principal ASC);
+CREATE INDEX fk_estabelecimento_moti1_idx ON estabelecimento (motivo_situacao_cadastral ASC);
+
+
+ALTER TABLE estabelecimento  WITH CHECK ADD  CONSTRAINT [fk_estabelecimento_pais] FOREIGN KEY([pais])
+REFERENCES pais ([codigo])
+ALTER TABLE estabelecimento CHECK CONSTRAINT [fk_estabelecimento_pais]
+
+ALTER TABLE estabelecimento  WITH CHECK ADD  CONSTRAINT fk_estabelecimento_munic1 FOREIGN KEY([municipio])
+REFERENCES munic ([codigo])
+ALTER TABLE estabelecimento CHECK CONSTRAINT fk_estabelecimento_munic1
+
+
+ALTER TABLE estabelecimento  WITH CHECK ADD  CONSTRAINT [fk_estabelecimento_cnae1] FOREIGN KEY([cnae_fiscal_principal])
+REFERENCES cnae ([codigo])
+ALTER TABLE estabelecimento CHECK CONSTRAINT [fk_estabelecimento_cnae1]
+ 
+
+ALTER TABLE estabelecimento  WITH CHECK ADD  CONSTRAINT [fk_estabelecimento_moti1] FOREIGN KEY([motivo_situacao_cadastral])
+REFERENCES moti ([codigo])
+ALTER TABLE estabelecimento CHECK CONSTRAINT [fk_estabelecimento_moti1]
+
+
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'COMMENT NÚMERO BASE DE INSCRIÇÃO NO CNPJ (OITO PRIMEIROS DÍGITOS DO CNPJ).' , @level0type=N'SCHEMA',@level0name= @schema_default_name, @level1type=N'TABLE',@level1name=N'ESTABELECIMENTO', @level2type=N'COLUMN',@level2name=N'cnpj_basico'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'NÚMERO DO ESTABELECIMENTO DE INSCRIÇÃO NO CNPJ (DO NONO ATÉ O DÉCIMO SEGUNDO DÍGITO DO CNPJ).' , @level0type=N'SCHEMA',@level0name= @schema_default_name, @level1type=N'TABLE',@level1name=N'ESTABELECIMENTO', @level2type=N'COLUMN',@level2name=N'cnpj_ordem'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'DÍGITO VERIFICADOR DO NÚMERO DE INSCRIÇÃO NO CNPJ (DOIS ÚLTIMOS DÍGITOS DO CNPJ).' , @level0type=N'SCHEMA',@level0name= @schema_default_name, @level1type=N'TABLE',@level1name=N'ESTABELECIMENTO', @level2type=N'COLUMN',@level2name=N'cnpj_dv'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'CÓDIGO DO IDENTIFICADOR MATRIZ/FILIAL:\n1 – MATRIZ\n2 – FILIAL' , @level0type=N'SCHEMA',@level0name= @schema_default_name, @level1type=N'TABLE',@level1name=N'ESTABELECIMENTO', @level2type=N'COLUMN',@level2name=N'identificador_matriz_filial'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'CORRESPONDE AO NOME FANTASIA' , @level0type=N'SCHEMA',@level0name= @schema_default_name, @level1type=N'TABLE',@level1name=N'ESTABELECIMENTO', @level2type=N'COLUMN',@level2name=N'nome_fantasia'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'CÓDIGO DA SITUAÇÃO CADASTRAL:\n01 – NULA\n2 – ATIVA\n3 – SUSPENSA\n4 – INAPTA\n08 – BAIXADA' , @level0type=N'SCHEMA',@level0name= @schema_default_name, @level1type=N'TABLE',@level1name=N'ESTABELECIMENTO', @level2type=N'COLUMN',@level2name=N'situacao_cadastral'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'DATA DO EVENTO DA SITUAÇÃO CADASTRAL' , @level0type=N'SCHEMA',@level0name= @schema_default_name, @level1type=N'TABLE',@level1name=N'ESTABELECIMENTO', @level2type=N'COLUMN',@level2name=N'data_situacao_cadastral'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'CÓDIGO DO MOTIVO DA SITUAÇÃO CADASTRAL' , @level0type=N'SCHEMA',@level0name= @schema_default_name, @level1type=N'TABLE',@level1name=N'ESTABELECIMENTO', @level2type=N'COLUMN',@level2name=N'motivo_situacao_cadastral'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'NOME DA CIDADE NO EXTERIOR' , @level0type=N'SCHEMA',@level0name= @schema_default_name, @level1type=N'TABLE',@level1name=N'ESTABELECIMENTO', @level2type=N'COLUMN',@level2name=N'nome_cidade_exterior'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'CÓDIGO DO PAIS' , @level0type=N'SCHEMA',@level0name= @schema_default_name, @level1type=N'TABLE',@level1name=N'ESTABELECIMENTO', @level2type=N'COLUMN',@level2name=N'pais'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'DATA DE INÍCIO DA ATIVIDADE' , @level0type=N'SCHEMA',@level0name= @schema_default_name, @level1type=N'TABLE',@level1name=N'ESTABELECIMENTO', @level2type=N'COLUMN',@level2name=N'data_inicio_atividade'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'CÓDIGO DA ATIVIDADE ECONÔMICA PRINCIPAL DO ESTABELECIMENTO' , @level0type=N'SCHEMA',@level0name= @schema_default_name, @level1type=N'TABLE',@level1name=N'ESTABELECIMENTO', @level2type=N'COLUMN',@level2name=N'cnae_fiscal_principal'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'CÓDIGO DA(S) ATIVIDADE(S) ECONÔMICA(S) SECUNDÁRIA(S) DO ESTABELECIMENTO' , @level0type=N'SCHEMA',@level0name= @schema_default_name, @level1type=N'TABLE',@level1name=N'ESTABELECIMENTO', @level2type=N'COLUMN',@level2name=N'cnae_fiscal_secundaria'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'tipo_logradouro' , @level0type=N'SCHEMA',@level0name= @schema_default_name, @level1type=N'TABLE',@level1name=N'ESTABELECIMENTO', @level2type=N'COLUMN',@level2name=N'tipo_logradouro'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'NOME DO LOGRADOURO ONDE SE LOCALIZA O ESTABELECIMENTO.' , @level0type=N'SCHEMA',@level0name= @schema_default_name, @level1type=N'TABLE',@level1name=N'ESTABELECIMENTO', @level2type=N'COLUMN',@level2name=N'logradouro'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'NÚMERO ONDE SE LOCALIZA O ESTABELECIMENTO. QUANDO NÃO HOUVER PREENCHIMENTO DO NÚMERO HAVERÁ ‘S/N’.' , @level0type=N'SCHEMA',@level0name= @schema_default_name, @level1type=N'TABLE',@level1name=N'ESTABELECIMENTO', @level2type=N'COLUMN',@level2name=N'numero'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'COMPLEMENTO PARA O ENDEREÇO DE LOCALIZAÇÃO DO ESTABELECIMENTO' , @level0type=N'SCHEMA',@level0name= @schema_default_name, @level1type=N'TABLE',@level1name=N'ESTABELECIMENTO', @level2type=N'COLUMN',@level2name=N'complemento'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'BAIRRO ONDE SE LOCALIZA O ESTABELECIMENTO.' , @level0type=N'SCHEMA',@level0name= @schema_default_name, @level1type=N'TABLE',@level1name=N'ESTABELECIMENTO', @level2type=N'COLUMN',@level2name=N'bairro'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'CÓDIGO DE ENDEREÇAMENTO POSTAL REFERENTE AO LOGRADOURO NO QUAL O ESTABELECIMENTO ESTA LOCALIZADO' , @level0type=N'SCHEMA',@level0name= @schema_default_name, @level1type=N'TABLE',@level1name=N'ESTABELECIMENTO', @level2type=N'COLUMN',@level2name=N'cep'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'SIGLA DA UNIDADE DA FEDERAÇÃO EM QUE SE ENCONTRA O ESTABELECIMENTO' , @level0type=N'SCHEMA',@level0name= @schema_default_name, @level1type=N'TABLE',@level1name=N'ESTABELECIMENTO', @level2type=N'COLUMN',@level2name=N'uf'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'CÓDIGO DO MUNICÍPIO DE JURISDIÇÃO ONDE SE ENCONTRA O ESTABELECIMENTO' , @level0type=N'SCHEMA',@level0name= @schema_default_name, @level1type=N'TABLE',@level1name=N'ESTABELECIMENTO', @level2type=N'COLUMN',@level2name=N'municipio'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'ddd_1' , @level0type=N'SCHEMA',@level0name= @schema_default_name, @level1type=N'TABLE',@level1name=N'ESTABELECIMENTO', @level2type=N'COLUMN',@level2name=N'ddd_1'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'telefone_1 ' , @level0type=N'SCHEMA',@level0name= @schema_default_name, @level1type=N'TABLE',@level1name=N'ESTABELECIMENTO', @level2type=N'COLUMN',@level2name=N'telefone_1'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'ddd_2' , @level0type=N'SCHEMA',@level0name= @schema_default_name, @level1type=N'TABLE',@level1name=N'ESTABELECIMENTO', @level2type=N'COLUMN',@level2name=N'ddd_2'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'telefone_2' , @level0type=N'SCHEMA',@level0name= @schema_default_name, @level1type=N'TABLE',@level1name=N'ESTABELECIMENTO', @level2type=N'COLUMN',@level2name=N'telefone_2'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'ddd_fax' , @level0type=N'SCHEMA',@level0name= @schema_default_name, @level1type=N'TABLE',@level1name=N'ESTABELECIMENTO', @level2type=N'COLUMN',@level2name=N'ddd_fax'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'fax' , @level0type=N'SCHEMA',@level0name= @schema_default_name, @level1type=N'TABLE',@level1name=N'ESTABELECIMENTO', @level2type=N'COLUMN',@level2name=N'fax'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'correio_eletronico' , @level0type=N'SCHEMA',@level0name= @schema_default_name, @level1type=N'TABLE',@level1name=N'ESTABELECIMENTO', @level2type=N'COLUMN',@level2name=N'correio_eletronico'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'situacao_especial' , @level0type=N'SCHEMA',@level0name= @schema_default_name, @level1type=N'TABLE',@level1name=N'ESTABELECIMENTO', @level2type=N'COLUMN',@level2name=N'situacao_especial'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'data_situacao_especial' , @level0type=N'SCHEMA',@level0name= @schema_default_name, @level1type=N'TABLE',@level1name=N'ESTABELECIMENTO', @level2type=N'COLUMN',@level2name=N'data_situacao_especial'
 
 -- -----------------------------------------------------
 -- Table 'simples'
