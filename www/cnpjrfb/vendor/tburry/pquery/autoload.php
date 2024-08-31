9d1f11953842e18f56bf48e1b1dc5f453f488891
bjverde <bjverde@yahoo.com.br>
2024-08-31 00:05:44 -0300

    🔥 #49 removendo arquivos vendor

--- www/cnpjrfb/vendor/tburry/pquery/autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/IQuery.php';
-require_once __DIR__ . '/gan_formatter.php';
-require_once __DIR__ . '/gan_node_html.php';
-require_once __DIR__ . '/gan_tokenizer.php';
-require_once __DIR__ . '/gan_parser_html.php';
-require_once __DIR__ . '/gan_selector_html.php';
-require_once __DIR__ . '/gan_xml2array.php';
-require_once __DIR__ . '/pQuery.php';
